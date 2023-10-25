5bb5dacac7370ee4c623c1d34f6ba8c9cdd6a46b
Matthew Giem <matthew.giem.programmer@gmail.com>
2023-10-25 13:44:41 +0700

    basic functionality

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from flask import Flask
+from random import randint
 app = Flask(__name__)
 
+number = randint(0,9)
 
 @app.route('/')
 def hello_world():
     return 'guess the number'
+
+@app.route("/<guess>")
+def guess_the_number(guess):
+    guess = int(guess)
+    if guess < number:
+        return f"{guess} is too low, guess higher"
+    if guess > number:
+        return f"{guess} is too high, guess lower"
+    else:
+        return f"Correct!! {guess} = {number}"
 
 if __name__ == "__main__":
     app.run()
